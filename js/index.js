dd737de714aaab67bbdf5eacbd36c27f1cacddbb
coolbeatz71 <sigmacool@gmail.com>
2021-03-08 21:09:48 +0200

    bug(expiry-date): fix bug on expiry date mask/pattern [fixes]

--- js/index.js
+++ js/index.js
@@ -5,17 +5,17 @@ const securitycode = document.getElementById("securitycode");
 var cardnumber_mask = new IMask(cardnumber, {
   mask: [
     {
-      mask: "0000 000000 00000",
+      mask: "0000-000000-00000",
       regex: "^3[47]\\d{0,13}",
       cardtype: "american express",
     },
     {
-      mask: "0000 0000 0000 0000",
+      mask: "0000-0000-0000-0000",
       regex: "^(?:6011|65\\d{0,2}|64[4-9]\\d?)\\d{0,12}",
       cardtype: "discover",
     },
     {
-      mask: "0000 000000 0000",
+      mask: "0000-000000-0000",
       regex: "^3(?:0([0-5]|9)|[689]\\d?)\\d{0,11}",
       cardtype: "diners",
     },
@@ -25,17 +25,17 @@ var cardnumber_mask = new IMask(cardnumber, {
       cardtype: "mastercard",
     },
     {
-      mask: "0000 000000 00000",
+      mask: "0000-000000-00000",
       regex: "^(?:2131|1800)\\d{0,11}",
       cardtype: "jcb15",
     },
     {
-      mask: "0000 0000 0000 0000",
+      mask: "0000-0000-0000-0000",
       regex: "^(?:35\\d{0,2})\\d{0,12}",
       cardtype: "jcb",
     },
     {
-      mask: "0000 0000 0000 0000",
+      mask: "0000-0000-0000-0000",
       regex: "^(?:5[0678]\\d{0,2}|6304|67\\d{0,2})\\d{0,12}",
       cardtype: "maestro",
     },
@@ -46,12 +46,12 @@ var cardnumber_mask = new IMask(cardnumber, {
       cardtype: "visa",
     },
     {
-      mask: "0000 0000 0000 0000",
+      mask: "0000-0000-0000-0000",
       regex: "^62\\d{0,14}",
       cardtype: "unionpay",
     },
     {
-      mask: "0000 0000 0000 0000",
+      mask: "0000-0000-0000-0000",
       cardtype: "Unknown",
     },
   ],
